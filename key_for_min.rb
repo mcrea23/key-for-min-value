5da1edd2a3f72075e69e459eace9c6d9a1119995
+ <mcrea23@aol.com>
2020-11-12 13:50:09 -0600

    commit tree 7048bc2f7d0410425ea0d40a12c2174b93c3a85c

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,15 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
-end
+    #:blake => 500, :ashley => 2, :adam => 1}
+    return nil if name_hash.size == 0
+    #get the first key:value pair by using .first method
+    min_key, min_value = name_hash.first
+    name_hash.each do |key, value|
+       # iterate over each value to check which one smaller than the return value
+       if value < min_value
+       min_key = key #just need the key
+       end
+    end
+    min_key
+ end
